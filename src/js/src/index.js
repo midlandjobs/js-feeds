b90ed5a43b2b4b35d898b1932405840b701af982
midlandjobs <rob.github.midlandjobs@gmail.com>
2023-06-30 20:39:19 +0100

    ...

--- src/js/src/index.js
+++ src/js/src/index.js
@@ -4,7 +4,7 @@ import Icons from 'uikit/dist/js/uikit-icons'; // import uikit icons
 UIkit.use(Icons); // use the Icon plugin
 window.UIkit = UIkit; // Make uikit available in window for inline scripts
 
-import {TwingEnvironment, TwingLoaderArray} from 'twing'; // for use in the browser
+import { TwingEnvironment, TwingLoaderArray } from 'twing'; // for use in the browser
 
 // import our twig templates specifically
 import baseTemplate from './templates/base.twig';
@@ -24,195 +24,195 @@ export default class MidlandJobsFeed {
 
 	#proxy;
 	#proxy_url;
-  #twing;
+	#twing;
 
-  constructor(sel, url, params = {remote: false}) {
+	constructor(sel, url, params = { remote: false }) {
 
-    // public
-    this.el = document.querySelector(sel);;
-    this.url = url;
-    this.params = params;
+		// public
+		this.el = document.querySelector(sel);;
+		this.url = url;
+		this.params = params;
 
-    // private
-    this.#proxy = 'https://test.com/projects/xmlproxy/xmlproxy.php';
-    if (this.isFeedUrlValid()) this.#proxy_url = encodeURI(this.#proxy) + "?url=" + encodeURI(this.url);
-    this.#twing = new TwingEnvironment(new TwingLoaderArray(templatesArray));
+		// private
+		this.#proxy = 'https://test.com/projects/xmlproxy/xmlproxy.php';
+		if (this.isFeedUrlValid()) this.#proxy_url = encodeURI(this.#proxy) + "?url=" + encodeURI(this.url);
+		this.#twing = new TwingEnvironment(new TwingLoaderArray(templatesArray));
 
-    // attempt to build the feed & place it
-    (async () => {
-      await this.buildFeedAll();
-    })();
-    
-  }
+		// attempt to build the feed & place it
+		(async () => {
+			await this.buildFeedAll();
+		})();
 
-  //
-  // Utility methods
-  // should these be public, private, static etc...
-  //
-  isObjectWithData(obj){
-    const isObjectEmpty = (_obj) => {
-      return (
-        _obj &&
-        Object.keys(_obj).length === 0 &&
-        _obj.constructor === Object
-      );
-    };
-    const isObject = (_obj) => {
-      return (
-        _obj != null && 
-        _obj.constructor.name === "Object"
-      );
-    };
-    if(isObject(obj)){
-      if(isObjectEmpty(obj)){
-        return false;
-      }
-      return true;
-    }
-    return false;
-  }
+	}
 
-  //
-  // Validation methods
-  // should these be public, private, static etc...
-  //
-  isFeedTargetAvailable() {
-    if(document.getElementById(this.id)) return true;
-    return false;
-  }
-  isFeedUrlValid() {
-    const isValidUrl = urlString=> {
-      var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
-      '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
-      return !!urlPattern.test(urlString);
-    }
-    if(this.url){
-      if(isValidUrl(this.url)) return true;
-    }
-    return false;
-  }
-  isFeedParamsValid() {
-    if(this.isObjectWithData(this.params)){
-      return true;
-    }
-    return false;
-  }
-  isHtmlValid(html) {
-    const regexForHTML = /<([A-Za-z][A-Za-z0-9]*)\b[^>]*>(.*?)<\/\1>/;
-    let is_valid = regexForHTML.test(html);
-    if(is_valid) return true
-    return false;
-  }
-  isElementValid(el) {
-    if (el && typeof(el) != 'undefined' && el != null) return true;
-    return false;
-  }
+	//
+	// Utility methods
+	// should these be public, private, static etc...
+	//
+	isObjectWithData(obj) {
+		const isObjectEmpty = (_obj) => {
+			return (
+				_obj &&
+				Object.keys(_obj).length === 0 &&
+				_obj.constructor === Object
+			);
+		};
+		const isObject = (_obj) => {
+			return (
+				_obj != null &&
+				_obj.constructor.name === "Object"
+			);
+		};
+		if (isObject(obj)) {
+			if (isObjectEmpty(obj)) {
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
 
-  //
-  // Other methods
-  // should these be public, private, static etc...
-  //
-  createContextFromJson(json) {
-    let context = {};
-    if(json.publisher) context.publisher = json.publisher;
-    if(json.publisherurl) context.publisher_url = json.publisherurl;
-    if(json.lastBuildDate) context.last_build_date = json.lastBuildDate;
-    if(json.job) context.jobs = json.job;
-    context.remote = this.params.remote;
-    return context;
-  }
+	//
+	// Validation methods
+	// should these be public, private, static etc...
+	//
+	isFeedTargetAvailable() {
+		if (document.getElementById(this.id)) return true;
+		return false;
+	}
+	isFeedUrlValid() {
+		const isValidUrl = urlString => {
+			var urlPattern = new RegExp('^(https?:\\/\\/)?' + // validate protocol
+				'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // validate domain name
+				'((\\d{1,3}\\.){3}\\d{1,3}))' + // validate OR ip (v4) address
+				'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // validate port and path
+				'(\\?[;&a-z\\d%_.~+=-]*)?' + // validate query string
+				'(\\#[-a-z\\d_]*)?$', 'i'); // validate fragment locator
+			return !!urlPattern.test(urlString);
+		}
+		if (this.url) {
+			if (isValidUrl(this.url)) return true;
+		}
+		return false;
+	}
+	isFeedParamsValid() {
+		if (this.isObjectWithData(this.params)) {
+			return true;
+		}
+		return false;
+	}
+	isHtmlValid(html) {
+		const regexForHTML = /<([A-Za-z][A-Za-z0-9]*)\b[^>]*>(.*?)<\/\1>/;
+		let is_valid = regexForHTML.test(html);
+		if (is_valid) return true
+		return false;
+	}
+	isElementValid(el) {
+		if (el && typeof (el) != 'undefined' && el != null) return true;
+		return false;
+	}
 
-  //
-  // Output/Async methods
-  // return promises/data
-  // uses await
-  // uses try/catch
-  //
+	//
+	// Other methods
+	// should these be public, private, static etc...
+	//
+	createContextFromJson(json) {
+		let context = {};
+		if (json.publisher) context.publisher = json.publisher;
+		if (json.publisherurl) context.publisher_url = json.publisherurl;
+		if (json.lastBuildDate) context.last_build_date = json.lastBuildDate;
+		if (json.job) context.jobs = json.job;
+		context.remote = this.params.remote;
+		return context;
+	}
 
-  // await fetchFeedJson & renderFeedJsonAsHtml. try/catch target ele & rendered html errors
-  async buildFeedAll() {
+	//
+	// Output/Async methods
+	// return promises/data
+	// uses await
+	// uses try/catch
+	//
 
-    let json;
-    let html;
+	// await fetchFeedJson & renderFeedJsonAsHtml. try/catch target ele & rendered html errors
+	async buildFeedAll() {
 
-    try {
+		let json;
+		let html;
 
-      json = await this.fetchFeedJson();
-      if(json){
-        html = await this.renderFeedJsonAsHtml(json);
-        if(this.isElementValid(this.el) && this.isHtmlValid(html)){
-          this.el.innerHTML = html;
-        } else {
-          if(!this.isElementValid(this.el)) throw new TypeError('A valid DOM node has not been provided');
-          if(!this.isHtmlValid(html)) throw new TypeError('Invalid html has been used to build the feed');
-        }
-      }
+		try {
 
-    }
-    catch (e) {
-      if (!(e instanceof Error)) {
-        e = new Error(e);
-      }
-      console.error(e.message);
-    }
+			json = await this.fetchFeedJson();
+			if (json) {
+				html = await this.renderFeedJsonAsHtml(json);
+				if (this.isElementValid(this.el) && this.isHtmlValid(html)) {
+					this.el.innerHTML = html;
+				} else {
+					if (!this.isElementValid(this.el)) throw new TypeError('A valid DOM node has not been provided');
+					if (!this.isHtmlValid(html)) throw new TypeError('Invalid html has been used to build the feed');
+				}
+			}
 
-  }
+		}
+		catch (e) {
+			if (!(e instanceof Error)) {
+				e = new Error(e);
+			}
+			console.error(e.message);
+		}
 
-  // await fetch, try/catch server/xml errors
-  async fetchFeedJson(report = true) {
+	}
 
-    let json;
+	// await fetch, try/catch server/xml errors
+	async fetchFeedJson(report = true) {
 
-    try {
+		let json;
 
-      const response = await fetch(this.#proxy_url);
-      json = await response.json();
+		try {
 
-      if(report){
-        if (!response.ok) throw new Error('A valid response was not returned from fetch. bad url given. ' + response.statusText);
-        if (!json || json.length == 0 || json.error == true) throw new Error('No json was returned. bad url given.');
-      }
+			const response = await fetch(this.#proxy_url);
+			json = await response.json();
 
-    }
-    catch (err) {
+			if (report) {
+				if (!response.ok) throw new Error('A valid response was not returned from fetch. bad url given. ' + response.statusText);
+				if (!json || json.length == 0 || json.error == true) throw new Error('No json was returned. bad url given.');
+			}
 
-      if(report){
-        if (err instanceof SyntaxError) console.log('There was a SyntaxError (Unexpected token < in JSON).', err);
-        else if (err instanceof TypeError) console.log('There was an issue connecting to the necessary server.', err);
-        else console.log('The response was not ok.', err)
-      }
+		}
+		catch (err) {
 
-    }
+			if (report) {
+				if (err instanceof SyntaxError) console.log('There was a SyntaxError (Unexpected token < in JSON).', err);
+				else if (err instanceof TypeError) console.log('There was an issue connecting to the necessary server.', err);
+				else console.log('The response was not ok.', err)
+			}
 
-    if (json) return json; // promise returned
-  }
+		}
 
-  // await render, try/catch rendering errors (json input & html out)
-  async renderFeedJsonAsHtml(json, report = true) {
+		if (json) return json; // promise returned
+	}
 
-    let html;
+	// await render, try/catch rendering errors (json input & html out)
+	async renderFeedJsonAsHtml(json, report = true) {
 
-    try {
-      const context = this.createContextFromJson(json);
-      html = await this.#twing.render('jobs.twig', context);
-      if(report){
-        if (!json || json.length == 0 || json.error == true) throw new Error('The json provided was invalid or empty.');
-        if (!html || html.length == 0 || !this.isHtmlValid(html)) throw new Error('There was an error rendering the feed.');
-      }
-    }
-    catch(err) {
-      if(report){
-       console.log(err);
-      }
-    }
+		let html;
 
-    if(html && this.isHtmlValid(html)) return html; // promise returned
-		
-  }
+		try {
+			const context = this.createContextFromJson(json);
+			html = await this.#twing.render('jobs.twig', context);
+			if (report) {
+				if (!json || json.length == 0 || json.error == true) throw new Error('The json provided was invalid or empty.');
+				if (!html || html.length == 0 || !this.isHtmlValid(html)) throw new Error('There was an error rendering the feed.');
+			}
+		}
+		catch (err) {
+			if (report) {
+				console.log(err);
+			}
+		}
+
+		if (html && this.isHtmlValid(html)) return html; // promise returned
+
+	}
 
 }
 window.MidlandJobsFeed = MidlandJobsFeed;
